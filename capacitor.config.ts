f207874e1b191646ac5932d22d839c3251d04af5
Ivan Martinez <ingeniero.ivan92@gmail.com>
2025-12-16 20:50:03 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hrapp.app',
+  appName: 'hr-app',
+  webDir: 'www'
+};
+
+export default config;
